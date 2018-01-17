90fd9f110438fa02f12b31a06453c2674b5829a0
Pedro Belo <pedrobelo@gmail.com>
2018-01-17 15:38:21 -0800

    react native 0.47 compat

--- android/app/src/main/java/com/reactnativenavigation/bridge/NavigationReactPackage.java
+++ android/app/src/main/java/com/reactnativenavigation/bridge/NavigationReactPackage.java
@@ -13,19 +13,16 @@ import java.util.List;
 
 public class NavigationReactPackage implements ReactPackage {
 
-    @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         return Arrays.<NativeModule>asList(
                 new NavigationReactModule(reactContext)
         );
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
 
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Arrays.<ViewManager>asList(
                 new SharedElementTransitionManager()
